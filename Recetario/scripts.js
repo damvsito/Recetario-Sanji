a9dadfd29fe062706ae1a5893e3aff47e43da51f
damvsito <151712464+damvsito@users.noreply.github.com>
2025-03-09 04:43:23 -0600

    Add files via upload

---
+++ Recetario/scripts.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById('search-button').addEventListener('click', function() {
+    alert('¡Busca tus recetas favoritas! Escribe el nombre en la barra de búsqueda.');
+});
